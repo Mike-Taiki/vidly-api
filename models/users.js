241b08f5ce9a59521cf802c70552d12cc8863f53
Michael Gomes <michael.21.2012@hotmail.com>
2019-10-26 13:59:28 -0300

    separating authenticate logic

--- models/users.js
+++ models/users.js
@@ -1,32 +1,46 @@
-const Joi = require('@hapi/joi');
-const mongoose = require('mongoose');
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
 
-const User = mongoose.model('User', new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 100,
-    },
-    email: {
-        type: String,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-    }
-}));
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 100
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  }
+});
+
+userSchema.methods.generateAuthToken = () => {
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(100).required(),
-        email: Joi.string().required(),
-        password: Joi.string().min(6).max(12),
-    })
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(5)
+      .max(100)
+      .required(),
+    email: Joi.string().required(),
+    password: Joi.string()
+      .min(6)
+      .max(12)
+  });
 
-    return schema.validate(user);
+  return schema.validate(user);
 }
 
 exports.User = User;
--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.post("/", async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  const token = await jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  const token = user.generateAuthToken();
   res
     .header("x-auth-token", token)
     .send(_.pick(user, ["_id", "name", "email"]));
